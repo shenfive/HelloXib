28df20313b81ef74cd2a6825c054dab4ce223f1a
Danny Shen <shenfive@gmail.com>
2019-11-30 17:03:38 +0800

    init

---
+++ HelloXib/MyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyView.swift
+//  HelloXib
+//
+//  Created by 申潤五 on 2019/11/30.
+//  Copyright © 2019 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
